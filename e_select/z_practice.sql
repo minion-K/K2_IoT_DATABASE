634a9a7b9fc6cf5d743dc2fd93f0b4546870618f
minion-K <rnjsalsgus06@naver.com>
2025-07-30 13:29:14 +0900

    SQL Practice

---
+++ e_select/z_practice.sql
@@ -0,0 +1,55 @@
+use korea_db;
+
+select * from `members`;
+select * from `purchases`;
+
+# 1)
+select name, contact, grade
+from `members`;
+
+# 2)
+select name, grade
+from `members`
+where area_code = 'SEOUL';
+
+# 3)
+select name, join_date, grade
+from `members`
+where grade in ('Gold','Platinum','Diamond');
+
+# 4)
+select name, join_date
+from `members`
+where year(join_date) = '2023';
+
+# 5)
+select name, points
+from `members`
+where points >= 100;
+
+# 6)
+select *
+from `members`
+where gender = 'Male' and grade in ('Gold', 'Platinum', 'Diamond');
+
+# 7)
+select member_id, max(purchase_date)
+from `purchases`
+group by member_id
+order by max(purchase_date) desc
+limit 3;
+
+# 8)
+select member_id, sum(amount)
+from `purchases`
+group by member_id;
+
+
+# 9)
+select M.member_id, M.name, sum(amount) total_amount
+from `members` M
+	join `purchases` P
+    on M.member_id = P.member_id
+group by P.member_id
+order by total_amount desc
+limit 1;
